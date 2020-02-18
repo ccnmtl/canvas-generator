1a2958d87496359f5ea3c2e93b14998e630cf722
bfasenfest <aowbackup@gmail.com>
2020-02-18 15:32:50 -0500

    First test of new export feature with xml for weeks

--- src/components/Export.vue
+++ src/components/Export.vue
@@ -124,6 +124,88 @@ export default {
 
       this.updateProp('url', this.parseUrl(this.info.url))
 
+      JSZipUtils.getBinaryContent('static/files/clean-course.imscc', (err, data) => {
+          if(err) {
+              throw err; // or handle err
+          }
+
+          JSZip.loadAsync(data).then((zip) => {
+            console.log(zip)
+            zip.file("wiki_content/home.html", headings.home + this.$refs.home.returnCode() + footer)
+            zip.file("wiki_content/weekly-activities.html", headings.list + this.$refs.list.returnCode() + footer)
+            zip.file("course_settings/syllabus.html", headings.syllabus + this.$refs.syllabus.returnCode() + footer);
+
+            let redirect_url = '<lticm:property name="url">' + this.info.url + 'pages/weekly-activities</lticm:property>'
+
+            zip.file("ccb-weekly-redirect.xml", headings.redirect_top + redirect_url + headings.redirect_bottom)
+            
+            // Add info to manifest
+            zip.file('imsmanifest.xml').async("string").then((data) => {
+            let parser = new DOMParser();
+            let manifest = parser.parseFromString(data, "text/xml")
+
+            //add weeks
+            for (let i=1; i <= this.weeks.length; i++){
+              let title = '<title>Week ' + i + '</title>'
+              let iden = '<meta name="identifier" content="ccb-week' + i + '"/>'
+              let el = document.getElementById("week-box" + (i-1))
+              let code = el.innerHTML.replace(/\bdata-v-\S+\"/ig,"")
+              zip.file("wiki_content/week-"+ i +".html", headings.top + title + iden + headings.bottom + code + footer)
+              addResource(manifest, "ccb-week-" + i, 'wiki_content/pages/week-' + i)
+            }
+
+            let serializer = new XMLSerializer();
+            let manifestString = serializer.serializeToString(manifest);
+
+            zip.file('imsmanifest.xml', manifestString )
+
+            zip.generateAsync({type:"blob"})
+            .then((blob) => {
+              saveFile({
+                name: this.info.title + '_export.imscc',
+                data: blob
+              })
+            });
+          
+            })
+
+          })
+      })
+
+      
+      function addResource(xml, iden, link, type="page", use=""){
+        let resource = xml.createElement("resource");
+        let file = xml.createElement("file");
+        resource.appendChild(file)
+
+        let resourceList = xml.getElementsByTagName("resources")[0]
+
+        resource.setAttribute("identifier", iden);
+
+        switch(type){
+          case "page":
+            resource.setAttribute("type", "webcontent");
+            resource.setAttribute("href", link);
+            file.setAttribute("href", link);
+            break;
+          case "discusson":
+            break;
+          default:
+            break;
+        }
+
+        resourceList.appendChild(resource)
+
+      }
+
+    },
+    // old imscc exporter
+    exportIMSCCOld () {
+
+      let footer = '</body> </html>'
+
+      this.updateProp('url', this.parseUrl(this.info.url))
+
       JSZipUtils.getBinaryContent('static/files/weekly-template.imscc', (err, data) => {
           if(err) {
               throw err; // or handle err
