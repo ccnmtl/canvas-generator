3cc55e1c53f77d62244091b10db71f79c3c13a5a
bfasenfest <aowbackup@gmail.com>
2023-03-01 22:46:43 -0500

    change default week text

--- src/store/components/defaults.js
+++ src/store/components/defaults.js
@@ -75,7 +75,7 @@ export default {
       date: moment(),
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus odio id aliquam vestibulum.",
       imgSrc: 'https://s3.us-east-2.amazonaws.com/sipa-canvas/canvas-images/session1' + '.png',
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus odio id aliquam vestibulum. Ut vel porttitor nibh. Cras arcu nunc, vehicula in blandit ut, imperdiet sed arcu. Mauris at dolor enim. Quisque sagittis quam nec ornare egestas. Fusce sed quam tempor, rhoncus urna sed, porttitor mauris. Etiam ac efficitur odio. Curabitur sodales pharetra sem, sit amet egestas nulla placerat non. Phasellus ex orci, sagittis viverra accumsan ut, lobortis eget odio. Pellentesque pretium mauris ante, et sodales justo fermentum ac. Suspendisse sed ultrices libero. Etiam sollicitudin pharetra rutrum.",
+      body: "In this module you will find a series of video lectures arrayed below. Please watch these lectures in order, from the top to the bottom of the page. Once you are finished with all of the lecture videos, please scroll to the bottom of the page to view any discussions, assignments, or quizes, or click <b>Next</b> to automatically proceed to the next module.",
       required: '<table class="ic-Table ic-Table--condensed"><thead><tr><th>Readings</th><th>Lecture Presentation</th><th></th><th></th></tr></thead><tbody><tr><td>Auftragstaktik</td><td>Managing to 2019</td></tr><tr><td>Drucker and Management </td><td>Memo Format</td></tr><tr><td>Action Verb List</td><td>Discussion</td></tr><tr><td>Memo Guide</td><td>Recitation</td></tr></tbody></table>',
       videos: [],
       discussions: [],
